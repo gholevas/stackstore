f74c5a0489bb858d55376b0946d9c43aba8705a8
George <gholevas1@gmail.com>
2016-03-01 10:56:58 -0500

    added some routes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,8 +1,43 @@
 'use strict';
 var router = require('express').Router();
+var mongoose = require('mongoose');
+
 module.exports = router;
 
 router.use('/members', require('./members'));
+
+// get one user
+router.get('/user/:userId', function (req, res) {
+    mongoose.model('User').findById(req.params.userId)
+    .then(function(info){
+        res.send(info);
+    })
+});
+
+
+// get all boxes
+router.get('/boxes/', function (req, res) {
+    mongoose.model('Box').find({})
+    .then(function(info){
+        res.send(info);
+    })
+});
+
+// add a boxes
+router.post('/boxes/', function (req, res) {
+    mongoose.model('Box').create(req.body)
+    .then(function(info){
+        res.send(info);
+    })
+});
+
+
+// Make sure this is after all of
+// the registered routes!
+router.use(function (req, res) {
+    res.status(404).end();
+});
+
 
 // Make sure this is after all of
 // the registered routes!
