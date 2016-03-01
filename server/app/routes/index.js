1e0bfcfb63f6170b43905484a087ca02358c815a
George <gholevas1@gmail.com>
2016-03-01 11:17:31 -0500

    made box routes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -6,26 +6,12 @@ module.exports = router;
 
 router.use('/members', require('./members'));
 
+router.use('/boxes', require('./boxes.js'));
+
+
 // get one user
 router.get('/user/:userId', function (req, res) {
     mongoose.model('User').findById(req.params.userId)
-    .then(function(info){
-        res.send(info);
-    })
-});
-
-
-// get all boxes
-router.get('/boxes/', function (req, res) {
-    mongoose.model('Box').find({})
-    .then(function(info){
-        res.send(info);
-    })
-});
-
-// add a boxes
-router.post('/boxes/', function (req, res) {
-    mongoose.model('Box').create(req.body)
     .then(function(info){
         res.send(info);
     })
