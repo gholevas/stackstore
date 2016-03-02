849b81de5bee1a4848837ac4c47cdcbec279fdbd
Gustaf Erickson <gerickson808@gmail.com>
2016-03-02 12:38:08 -0500

    Built User Profile directive

---
+++ browser/js/members-only/user-profile.directive.js
@@ -0,0 +1,27 @@
+app.directive('userProfile', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/members-only/user-profile.directive.html',
+        controller: function($scope, $mdDialog){
+        	$scope.showTabDialog = function(ev) {
+			    $mdDialog.show({
+			      controller: function DialogController($scope, $mdDialog) {
+						  $scope.hide = function() {
+						    $mdDialog.hide();
+						  };
+						  $scope.cancel = function() {
+						    $mdDialog.cancel();
+						  };
+						  $scope.answer = function(answer) {
+						    $mdDialog.hide(answer);
+						  };
+						},
+			      templateUrl: 'tabDialog.tmpl.html',
+			      parent: angular.element(document.body),
+			      targetEvent: ev,
+			      clickOutsideToClose:true
+			    });
+        };
+    }
+  };
+});
