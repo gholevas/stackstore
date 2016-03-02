7df3989a33432da74eeec55df83b00f0c099f710
elpenao <elpenao@gmail.com>
2016-03-02 16:57:58 -0500

    take out console log

--- browser/js/cart/cart-controller.js
+++ browser/js/cart/cart-controller.js
@@ -3,7 +3,6 @@ app.controller('CartController', function($scope, $mdSidenav, CartFactory) {
     CartFactory.getUserCart()
         .then(function(cart) {
             $scope.cart = cart
-            console.log("$scope.cart", $scope.cart.boxes[0].box[0].name)
         })
 
     $scope.range = function(n) {
