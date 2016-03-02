0f65c11757c26a6c2fe98249c602b105fa654433
prakashmallela <prakash.mallela@gmail.com>
2016-03-02 15:07:03 -0500

    name unique error

--- seed.js
+++ seed.js
@@ -75,11 +75,10 @@ connectToDb
         ,isAdmin: false
         ,currentCart: cart._id
     }
-    // , {
-    //     email: 'obama@gmail.com'
-    //     ,password: 'potus'
-    //     ,isAdmin: true
-    //     ,currentCart: null}
+    , {
+        email: 'obama@gmail.com'
+        ,password: 'potus'
+        ,isAdmin: true}
     ];
 
     return User.createAsync(users);
