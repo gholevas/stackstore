7459d574482f41b7f2d75db37462e09be0fe4840
George <gholevas1@gmail.com>
2016-03-03 11:54:22 -0500

    minor edits

--- server/app/routes/answers.js
+++ server/app/routes/answers.js
@@ -1,6 +1,7 @@
 'use strict';
 var router = require('express').Router();
 var mongoose = require('mongoose');
+var Box = mongoose.model('Box');
 module.exports = router;
 
 
@@ -9,6 +10,7 @@ module.exports = router;
 // post answers
 router.post('/', function (req, res, next) {
     console.log(req.body)
+    Box.findOne()
     // mongoose.model('Question').create(req.body)
     // .then(function(info){
     //     res.json(info);
