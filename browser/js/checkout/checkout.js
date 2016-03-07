8bbc3d9ad9bc121ad4b82e783e7bb2061ea6b941
Gustaf Erickson <gerickson808@gmail.com>
2016-03-07 15:01:30 -0500

    Added states to checkout

--- browser/js/checkout/checkout.js
+++ browser/js/checkout/checkout.js
@@ -19,7 +19,7 @@ app.controller('CheckOutCtrl', function($rootScope, $scope, $state, user, CartFa
 
     $scope.user = user;
     $scope.cart = cart;
-
+    $scope.states = "Alabama,Alaska,Arizona,Arkansas,California,Colorado,Connecticut,Delaware,Florida,Georgia,Hawaii,Idaho,Illinois,Indiana,Iowa,Kansas,Kentucky,Louisiana,Maine,Maryland,Massachusetts,Michigan,Minnesota,Mississippi,Missouri,Montana,Nebraska,Nevada,New Hampshire,New Jersey,New Mexico,New York,North Carolina,North Dakota,Ohio,Oklahoma,Oregon,Pennsylvania,Rhode Island,South Carolina,South Dakota,Tennessee,Texas,Utah,Vermont,Virginia,Washington,West Virginia,Wisconsin,Wyoming".split(",");
     $scope.placeOrder = function() {
         
         CartFactory.processOrder({
